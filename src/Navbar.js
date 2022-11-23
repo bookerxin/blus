64c041587b278119b81591dee07c3815d73ab70e
AD <AD@gmail.com>
2022-11-23 14:04:08 +0000

    need to reformat structure

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import * as ReactDom from 'react-dom/client';
+import {Button, ButtonGroup} from "react-bootstrap";
+
+export class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            temp: 10
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>lol</h1>
+            </div>
+        );
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar} from './Navbar';
 import React, { useState } from 'react';
 import * as ReactDom from 'react-dom/client';
-import * as bootstrap from 'react-bootstrap';
-import {Button} from "react-bootstrap";
+import {Button, ButtonGroup, Nav} from "react-bootstrap";
 
-const Example = () =>{
-    return (
-        <>
-            <Button as='a' variant={'primary'}>Main</Button>
-            <Button as='a' variant={'dark'}>Second</Button>
-        </>
-    );
-}
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<Example />);
+root.render(<Navbar />);
