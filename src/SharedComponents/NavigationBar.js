59eca943a1408ffd9c33bd70f657286bb485c906
AD <AD@gmail.com>
2022-11-24 09:31:21 +0000

    removed bootstrap; first commit

--- src/SharedComponents/NavigationBar.js
+++ src/SharedComponents/NavigationBar.js
@@ -1,19 +1,6 @@
 
-import {
-    Navbar,
-    Button,
-    ButtonGroup,
-    Container,
-    Nav,
-    NavbarBrand,
-    Accordion,
-    NavLink,
-    NavDropdown, Collapse, Row, Col
-} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React, { useState } from 'react';
-import NavbarCollapse from "react-bootstrap/NavbarCollapse";
-import DropdownItem from "react-bootstrap/DropdownItem";
 
 const NavigationBar = () => {
     const [time, setTime] = useState(new Date().toTimeString().split(' ')[0]);
@@ -25,35 +12,7 @@ const NavigationBar = () => {
     );
 
     return (
-        <Container>
-            <Row>
-                <Navbar bg='light' expand='lg'>
-                    <Container>
-                        <NavbarBrand>B+</NavbarBrand>
-                        <Col className='col-8'>
-                            <Container>
-                                <Nav className='md-auto'>
-                                    <NavLink>Home</NavLink>
-                                    <NavLink>Books</NavLink>
-                                </Nav>
-                            </Container>
-                        </Col>
-                        <Col className='col-1'>
-                            <NavbarCollapse aria-controls='basic-dropdown'>
-                                <NavDropdown className='' id='basic-dropdown' title='Worlds'>
-                                    <DropdownItem href='#'>Test World</DropdownItem>
-                                    {/*<NavDropdown.Divider />*/}
-                                    <DropdownItem href='#'>World of Goo</DropdownItem>
-                                </NavDropdown>
-                            </NavbarCollapse>
-                        </Col>
-                        <Col className='col-1'>
-                            <Nav>{time}UK</Nav>
-                        </Col>
-                    </Container>
-                </Navbar>
-            </Row>
-        </Container>
+        <h1>noboot</h1>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './SharedComponents/NavigationBar';
 import React, { useState } from 'react';
 import * as ReactDom from 'react-dom/client';
