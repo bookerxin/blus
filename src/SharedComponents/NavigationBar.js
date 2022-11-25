f66447e3bfbcd4933f5264b9f7b5d365f9a9bc4b
AD <AD@gmail.com>
2022-11-25 16:29:10 +0000

    attempting to use JS animations

--- src/SharedComponents/NavigationBar.js
+++ src/SharedComponents/NavigationBar.js
@@ -1,9 +1,11 @@
 
 import React, { useState } from 'react';
-import './SharedComponentsCSS/NavigationBar.css';
+import './CSS/NavigationBar.css';
 
 const NavigationBar = () => {
     const [time, setTime] = useState(new Date().toTimeString().split(' ')[0]);
+    const [navDir, setDir] = useState('up');
+    const [navPos, setPos] = useState(20);
 
     const timerId = setInterval(
         () => {
@@ -11,14 +13,38 @@ const NavigationBar = () => {
         }, 1000
     );
 
+    const navMovement = setInterval(
+        () => {
+            const navigation_bar = document.getElementsByClassName('navigation-bar-container')[0];
+
+            if (navDir === 'up') {
+                setPos(navPos-1);
+                if (navPos <= 0) {
+                    setDir('down')
+                }
+            }
+            else if (navDir === 'down') {
+                setPos(navPos+1);
+                if (navPos >= 30) {
+                    setDir('up')
+                }
+            }
+            console.log(navPos);
+            navigation_bar.style.top = navPos;
+        }, 2000
+    );
+
     return (
-        <div className='navigation-bar'>
-            <span className='navigation-brand'>blus</span>
-            <nav className='nav-links-container'>
-                <a href='#' className='nav-links'>Vakutter</a>
-                <a href='#' className='nav-links'>Derival</a>
-                <a href='#' className='nav-links'>Kossa</a>
-            </nav>
+        <div className='navigation-bar-container'>
+            <div className='navigation-bar'>
+                <span className='navigation-brand'>blus</span>
+                <nav className='nav-links-container'>
+                    <a href='#' className='nav-links'>Vakutter</a>
+                    <a href='#' className='nav-links'>Derival</a>
+                    <a href='#' className='nav-links'>Kossa</a>
+                </nav>
+                <span className='nav-clock'>{time}</span>
+            </div>
         </div>
     );
 }
@@ -27,5 +53,5 @@ const NavigationBar = () => {
 export default NavigationBar;
 
 // TODO - Add clock in top right
-// TODO - Add scrollable text box, with fade on top and bottom?
-// TODO - Play more with the styles; learn Bootstrap
+// TODO - Make navbar stick at top of screen on scroll.
+// TODO - Fix interval animation attempt lol.
--- src/index.js
+++ src/index.js
@@ -7,3 +7,6 @@ import * as ReactDom from 'react-dom/client';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(<NavigationBar />);
+
+// TODO - Add scrollable text box, with fade on top and bottom? New component.
+// TODO - Consider an alternative or some react link if we want changing colour schemes.
